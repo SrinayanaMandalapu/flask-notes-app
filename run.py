fc164db910f9c4a7cfef3fbe5c87a5e0cb720c6a
SrinayanaMandalapu <mandalapunayana@gmail.com>
2025-07-16 22:17:18 +0530

    Added SQLAlchemy setup and User model

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
-from flask import Flask, render_template
-import os
+from app import create_app
 
-app = Flask(__name__, template_folder=os.path.join('app', 'templates'))
-
-@app.route('/')
-def home():
-    return render_template('home.html')
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
