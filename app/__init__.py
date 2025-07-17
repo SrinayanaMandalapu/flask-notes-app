e24849f2e2eee5bc3f5022f02f95856385b91120
SrinayanaMandalapu <mandalapunayana@gmail.com>
2025-07-17 07:54:16 +0530

    Added User Authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app():
     app = Flask(__name__)
@@ -9,8 +12,18 @@ def create_app():
     app.config['SECRET_KEY'] = 'dev'
 
     db.init_app(app)
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     from .routes import main
+    from .auth import auth as auth_blueprint
+
     app.register_blueprint(main)
+    app.register_blueprint(auth_blueprint)
 
     return app
